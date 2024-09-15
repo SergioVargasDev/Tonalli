40d1b359d887cb48a25df13d8a66bf01a662bfd7
mauriciopereag <mauriciopereagzz@gmail.com>
2024-09-15 04:00:53 -0600

    Text-Speech con JSON

--- Speech-Text/speech-text.py
+++ Speech-Text/speech-text.py
@@ -1,23 +1,30 @@
+import json
 from google.cloud import speech
 
+# Crear el cliente de Google Cloud Speech
 client = speech.SpeechClient.from_service_account_file('key.json')
 
+# Leer el archivo MP3
 file_name = 'test.mp3'
 with open(file_name, 'rb') as f:
     mp3_data = f.read()
 
+# Configurar el archivo de audio y las opciones de reconocimiento
 audio_file = speech.RecognitionAudio(content=mp3_data)
-
 config = speech.RecognitionConfig(
     sample_rate_hertz=44100,
     enable_automatic_punctuation=True,
     language_code='es-MX'
 )
 
+# Realizar el reconocimiento
 response = client.recognize(
-    config=config, 
+    config=config,
     audio=audio_file
 )
 
+# Extraer el texto reconocido y construir el JSON
 for result in response.results:
-    print(result.alternatives[0].transcript)
+    transcript = result.alternatives[0].transcript
+    json_response = json.dumps({"text": transcript})
+    print(json_response)
